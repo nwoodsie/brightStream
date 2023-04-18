8c8c688ccd38ec6adc45738991029f02744a53ee
kombatwongbat <michael.hm.wong@gmail.com>
2023-04-18 17:19:50 +1000

    class page
    class page

--- src/components/LandingPages/ClassPage/ClassPage.js
+++ src/components/LandingPages/ClassPage/ClassPage.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import "./ClassPage.css"
+import { DefaultButton } from '../../index.js'
 
+const classtext = "Tools provided for educators and students to manage classrooms more effectively, such as the ability to track attendance and take live notes during a lecture. Streams and notes can also be saved to watch and review at any time. Tools provided for educators and students to manage classrooms more effectively, such as the ability to track attendance and take live notes during a lecture. Streams and notes can also be saved to watch and review at any time. "
 function ClassPage() {
   return (
-    <div className='ClassWrapper'>
-        <h1>Class Page</h1>
-    </div>
-  )
-}
+    <div className='classWrapper'>
+     <div className='classContainer'>
+     <div className='classLeft'>
+     <img src={require('./ClassPageImage.jpg')} alt=''></img>
+        </div>
+      <div className='classRight'>
+        <div className='classTitle'>
+             Feels more like a <br/> classroom
+        </div>
+        <div className='classText'>
+            {classtext}
+          </div>
+          <div className='buttonContainer'>
+            <DefaultButton text='Get started' size='Big'/>
+          </div>
 
-export default ClassPage;
+        
+
+      </div>
+     </div>
+    </div>
+    )
+  }
+  
+  export default ClassPage
