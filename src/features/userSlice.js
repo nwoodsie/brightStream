9e1dc388921c38ca74db89ffd24b79f71cee8583
Noah <noahnwoods@gmail.com>
2023-05-04 01:10:59 +1000

    Changes to landing page, add on hover and selected NavBar functionality

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,22 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-    //name of the slice
-    name:"user",
-    //initial state of the information that we want to track
-    initialState:{
-        user:null,
+  //name of the slice
+  name: "user",
+  //initial state of the information that we want to track
+  initialState: {
+    user: null,
+    selected: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
     },
-    reducers:{
-        login: (state,action) => {
-            state.user = action.payload;
-        },
-        logout: (state) => {
-            state.user = null;
-        }
-    }
-})
+    logout: (state) => {
+      state.user = null;
+    },
+    selectPage: (state, action) => {
+      state.selected = action.payload;
+    },
+  },
+});
 
-export const {login, logout} = userSlice.actions;
-export const selectUser = (state) => state.user.user
+export const { login, logout, selectPage } = userSlice.actions;
+export const selectUser = (state) => state.user.user;
+export const selectedPage = (state) => state.user.selected;
 export default userSlice.reducer;
