03ac1335836dfd77fcbe8ca6b83a2f22c9ddf79a
kombatwongbat <97073551+kombatwongbat@users.noreply.github.com>
2023-05-08 21:39:41 +1000

    new
    new

--- src/components/LandingPages/ClassPage/ClassPage.js
+++ src/components/LandingPages/ClassPage/ClassPage.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./ClassPage.css"
 import { DefaultButton } from '../../index.js'
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectPage } from "../../../features/userSlice.js";
 
 const classtext = "Tools provided for educators and students to manage classrooms more effectively, such as the ability to track attendance and take live notes during a lecture. Streams and notes can also be saved to watch and review at any time. Tools provided for educators and students to manage classrooms more effectively, such as the ability to track attendance and take live notes during a lecture. Streams and notes can also be saved to watch and review at any time. "
 function ClassPage() {
+
+  const dispatch = useDispatch();
+  const onNavClick = (nav) => {
+    dispatch(selectPage(nav));
+  };
   return (
     <div className='classWrapper'>
      <div className='classContainer'>
@@ -19,7 +27,9 @@ function ClassPage() {
             </div>
     
             <div className='classButtonContainer'>
-            <DefaultButton text='Get started' size='Big'/>
+            <Link to="/GetStarted" style={{ textDecoration: "none" }}>
+          <DefaultButton text="Get Started" size= "Big" onClick={() => onNavClick(null)} />
+        </Link>
           
           </div>
 
--- src/components/LandingPages/DolbyPage/DolbyPage.js
+++ src/components/LandingPages/DolbyPage/DolbyPage.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./DolbyPage.css";
 import { DefaultButton } from "../../index.js";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectPage } from "../../../features/userSlice.js";
+
 
 const Dolbytext =
   "The Dolby.io Streaming REST API allows remote management of your Dolby.io Real-time Streaming account. To enable remote access you must first use the Token system to securely authenticate your applications with Dolby.io Real-time Streaming. o acquire an API Token, log into your Dolby.io Real-time Streaming account and create an API Secret to be verified as a user, otherwise all API calls will be rejected.";
 
 function DolbyPage() {
+
+  const dispatch = useDispatch();
+  const onNavClick = (nav) => {
+    dispatch(selectPage(nav));
+  };
   return (
     <div className="dolbyWrapper">
       <div className="dolbyContainer">
@@ -15,7 +24,9 @@ function DolbyPage() {
           </div>
           <div className="dolbyText">{Dolbytext}</div>
           <div className="dolbybuttonContainer">
-            <DefaultButton text="Get started" size="Big" />
+          <Link to="/GetStarted" style={{ textDecoration: "none" }}>
+          <DefaultButton text="Get Started" size= "Big" onClick={() => onNavClick(null)} />
+        </Link>
           </div>
         </div>
         <div className="dolbyRight">
