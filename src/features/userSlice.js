4910791cfc6f112d96f420897534bca654c6f250
Noah <noahnwoods@gmail.com>
2023-05-11 03:49:49 +1000

    Browse Streams hooked up to firestore

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -7,8 +7,13 @@ export const userSlice = createSlice({
   initialState: {
     user: null,
     selected: null,
-    streamId: null,
-    streamName: null,
+    stream: {
+      streamId: null,
+      streamName: null,
+      streamDisplayName: null,
+      streamTitle: null,
+      streamTopic: null,
+    },
   },
   reducers: {
     login: (state, action) => {
@@ -21,8 +26,11 @@ export const userSlice = createSlice({
       state.selected = action.payload;
     },
     setCurrentStream: (state, action) => {
-      state.streamId = action.payload.userId;
+      state.streamId = action.payload.streamId;
       state.streamName = action.payload.streamName;
+      state.streamDisplayName = action.payload.streamDisplayName;
+      state.streamTitle = action.payload.streamTitle;
+      state.streamTopic = action.payload.streamTopic;
     },
   },
 });
@@ -31,4 +39,5 @@ export const { login, logout, selectPage, setCurrentStream } =
   userSlice.actions;
 export const selectUser = (state) => state.user.user;
 export const selectedPage = (state) => state.user.selected;
+export const selectStream = (state) => state.user.stream;
 export default userSlice.reducer;
