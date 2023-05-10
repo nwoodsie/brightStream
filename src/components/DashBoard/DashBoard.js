aa6b1c1b6afff1e68548d3e2c37c8aa904a91718
Noah <noahnwoods@gmail.com>
2023-05-11 06:18:38 +1000

    finish everything

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./DashBoard.css";
 import SideBar from "./SideBar.js";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 
 function DashBoard() {
-  const name = "Name"; // Get this name from firestore
+  const user_id = useSelector(selectUser).user;
+  const docRef = doc(db, "users", user_id);
+  const [userName, setUserName] = useState("name");
+
+  async function getUserData() {
+    const userdata = await getDoc(docRef);
+    if (userdata.exists()) {
+      const data = userdata.data();
+      setUserName(data.name);
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   return (
     <div className="dashBoardWrapper">
       <div className="dashBoardContainer">
         <SideBar />
         <div className="dashBoardMain">
-          <div className="dashBoardTitle">Welcome {name}</div>
+          <div className="dashBoardTitle">Welcome {userName}</div>
         </div>
       </div>
     </div>
